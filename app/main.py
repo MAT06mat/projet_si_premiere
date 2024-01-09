b22e6a9c2da0c2bb06295aa973e249ced894b974
Matthieu <matthieu.felten22@gmail.com>
2024-01-09 17:26:03 +0100

    Create base of app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from kivy.app import App
+from kivy.core.window import Window
 
 
 class VeloApp(App):
-    pass
+    icon = "images/icon.png"
+    
+    def build(self):
+        Window.clearcolor = (1, 1, 1, 1)
 
 
-VeloApp().run()
+if __name__ == '__main__':
+    VeloApp().run()
